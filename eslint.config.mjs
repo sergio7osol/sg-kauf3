fe438ecef59f4b336493f9d05f76960dbd36e5e3
Sergey Osokin <sergey.osokin@netfire.eu>
2025-11-02 01:28:53 +0100

    feat: initialize project with dashboard template setup.

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'vue/no-multiple-template-root': 'off',
+    'vue/max-attributes-per-line': ['error', { 
+      singleline: 1,
+      multiline: 1 
+    }]
+  }
+})
---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-07-11',
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@vueuse/nuxt'
+  ],
+  
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost',
+      appURL: 'http://localhost:3000',
+    },
+  },
+
+  routeRules: {
+    '/api/**': {
+      cors: true
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
